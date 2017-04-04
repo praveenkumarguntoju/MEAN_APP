1838ecafb4fcd0f873c2f39bd45cf0622951476f
praveenkumarguntoju <chintu1234>
2017-04-04 16:52:55 +0530

    latest changes on 15

--- proxy.js
+++ proxy.js
@@ -4,7 +4,7 @@ var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
 var serverOne = 'http://localhost:8080';
     app.all('*', function (req, res) {
-        console.log('redirecting to Server3');
-        apiProxy.web(req, res, {target: ServerThree});
+
+        apiProxy.web(req, res, {target: serverOne});
 });
 app.listen(3000);
