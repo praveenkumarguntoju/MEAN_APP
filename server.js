07245bb27252e9de7c9c5855981438d955636561
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-03 17:28:51 +0530

    update

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ switch(action) {
 // });
 
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8082, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
